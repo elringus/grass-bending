41067f1d17e9185e060c0c883fa002aa4cdd75ef
Artyom Sovetnikov <elringus@gmail.com>
2021-03-17 16:20:36 +0300

    publish

---
+++ Runtime/BenderPriorityComparer.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace GrassBending
+{
+    public sealed class BenderPriorityComparer : IComparer<IGrassBender>
+    {
+        public int Compare (IGrassBender x, IGrassBender y)
+        {
+            if (ReferenceEquals(x, y)) return 0;
+            if (ReferenceEquals(null, y)) return 1;
+            if (ReferenceEquals(null, x)) return -1;
+            return x.Priority.CompareTo(y.Priority);
+        }
+    }
+}
--- Runtime/GrassBendingManager.cs
+++ Runtime/GrassBendingManager.cs
@@ -1,5 +1,4 @@
 ﻿using System.Collections.Generic;
-using System.Linq;
 using UnityEngine;
 
 namespace GrassBending
@@ -11,53 +10,49 @@ namespace GrassBending
     {
         private class ProxyBehaviour : MonoBehaviour
         {
-            public System.Action OnUpdate { get; set; }
-            private void Update () => OnUpdate?.Invoke();
+            private void Update () => ApplyBending();
+            private void OnDestroy () => benders.Clear();
         }
 
         private const int sourcesLimit = 16;
-        private static readonly HashSet<IGrassBender> benders = new HashSet<IGrassBender>();
+        private static readonly BenderPriorityComparer comparer = new BenderPriorityComparer();
+        private static readonly List<IGrassBender> benders = new List<IGrassBender>();
         private static readonly Vector4[] bendData = new Vector4[sourcesLimit];
         private static readonly int bendDataPropertyId = Shader.PropertyToID("_BendData");
 
+        public static IReadOnlyList<IGrassBender> GetBenders () => benders;
+
         public static void AddBender (IGrassBender bender)
         {
-            if (!benders.Add(bender)) return;
-
-            // SortedSet generates garbage on enumeration, so hacking with linq here.
-            var sortedBenders = benders.OrderBy(b => b.Priority).ToList();
-            benders.Clear();
-            benders.UnionWith(sortedBenders);
+            // Sorted collections allocate garbage on enumeration; using list instead.
+            // The list length is expected to be minimal, so the overhead is negligible.
+            benders.Add(bender);
+            benders.Sort(comparer);
         }
 
-        public static void RemoveBender (IGrassBender bender) => benders.Remove(bender);
+        public static bool RemoveBender (IGrassBender bender) => benders.Remove(bender);
 
-        private static void ProcessBenders ()
+        private static void ApplyBending ()
         {
-            var sourceIndex = 0;
-            foreach (var bender in benders)
-            {
-                if (sourceIndex >= sourcesLimit) break;
-                bendData[sourceIndex] = new Vector4(bender.Position.x, bender.Position.y, bender.Position.z, bender.BendRadius);
-                sourceIndex++;
-            }
-
-            for (int i = sourceIndex; i < bendData.Length; i++)
-                bendData[i] = Vector4.zero;
-
+            for (int i = 0; i < bendData.Length; i++)
+                bendData[i] = GetBendDataAt(i);
             Shader.SetGlobalVectorArray(bendDataPropertyId, bendData);
+        }
+
+        private static Vector4 GetBendDataAt (int index)
+        {
+            if (index >= benders.Count) return Vector4.zero;
+            var bender = benders[index];
+            return new Vector4(bender.Position.x, bender.Position.y, bender.Position.z, bender.BendRadius);
         }
 
         [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
         private static void Initialize ()
         {
-            var objectName = nameof(GrassBendingManager);
-            var gameObject = new GameObject(objectName);
+            var gameObject = new GameObject(nameof(GrassBendingManager));
+            gameObject.AddComponent<ProxyBehaviour>();
             gameObject.hideFlags = HideFlags.HideAndDontSave;
             Object.DontDestroyOnLoad(gameObject);
-
-            var proxyBehaviour = gameObject.AddComponent<ProxyBehaviour>();
-            proxyBehaviour.OnUpdate = ProcessBenders;
         }
     }
 }
